9cda34f4f5620ab7e05f945c4d8c076180c754eb
孙彦飞 <1599782246@qq.com>
2016-09-19 12:00:34 +0800

    项目A 创建工程

---
+++ app/src/main/java/com/example/dllo/giftssayingapp/StartMainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dllo.giftssayingapp;
+
+public class StartMainActivity extends StartBaseActivity {
+
+
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_start_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
